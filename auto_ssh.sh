be47ed1a7365c32e3c947d79e4a8ada0c668e28b
Zepp <luzepu678@gmail.com>
2022-09-12 00:07:02 +0800

    Update auto_ssh.sh

--- auto_ssh.sh
+++ auto_ssh.sh
@@ -8,12 +8,16 @@ if [ -f $host_key_bk ]; then
     ln -sf $host_key_bk $host_key
 fi
 
-# 当前固件为稳定版时，需要执行下面命令开启SSH
+# 当前固件为 release 版时，需要执行下面命令开启SSH
 channel=`/sbin/uci get /usr/share/xiaoqiang/xiaoqiang_version.version.CHANNEL`
 if [ "$channel" = "release" ]; then
-    sed -i 's/channel=.*/channel="debug"/g' /etc/init.d/dropbear
+    sed -i 's/channel=.*/channel="stable"/g' /etc/init.d/dropbear
     /etc/init.d/dropbear restart
 fi
+
+# host key 没必要备份
+# TODO: 改成自动备份 /etc/dropbear/authorized_keys
+exit 0
 
 # 备份SSH密钥
 if [ ! -s $host_key_bk ]; then
